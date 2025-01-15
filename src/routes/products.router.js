0e28cc15b4028f4920a81d7253aa8f25d946d375
Juampi Cabrera <juampibariloche@gmail.com>
2025-01-14 23:05:53 -0300

    rutes

--- src/routes/products.router.js
+++ src/routes/products.router.js
@@ -1,13 +1,15 @@
 import { Router } from "express"
 import ProductManager from "../managers/product.manager.js";
+import { __dirname } from "../utils.js";
+import { socketServer } from "../app.js";
 
 
 const router = Router()
-
+const productManager = new ProductManager(__dirname + '/data/products.json');
 
 router.get("/", async (req, res) => {
     try {
-        const prods = await ProductManager.getAll();
+        const prods = await productManager.getAll();
         res.status(200).json(prods);
     } catch (error) {
         res.status(500).json({ message: error.message });
@@ -16,7 +18,7 @@ router.get("/", async (req, res) => {
 
 router.get("/", async (req, res) => {
     try {
-        const prods = await ProductManager.getById();
+        const prods = await productManager.getById();
         res.status(200).json(prods);
     } catch (error) {
         res.status(500).json({ message: error.message });
@@ -26,7 +28,7 @@ router.get("/", async (req, res) => {
 router.post('/', async (req,res)=>{
     const newProduct = req.body
     await ProductManager.create(newProduct)
-    const productsList = await ProductManager.getAll()
+    const productsList = await productManager.getAll()
     socketServer.emit('realtime', productsList)
     res.status(201).json(newProduct)
 
@@ -35,16 +37,16 @@ router.post('/', async (req,res)=>{
 router.put('/:pid', async (req,res)=>{
     const id = req.params.pid
     const newProduct = req.body
-    await ProductManager.update(newProduct,id)
-    const productsList = await ProductManager.getAll()
+    await productManager.update(newProduct,id)
+    const productsList = await productManager.getAll()
     socketServer.emit('realtime', productsList)
     res.status(201).json(newProduct)
 })
 
 router.delete('/:pid', async (req,res)=>{
     const id = req.params.pid
-    await ProductManager.delete(id)
-    const productsList = await ProductManager.getAll()
+    await productManager.delete(id)
+    const productsList = await productManager.getAll()
     socketServer.emit('realtime', productsList)
     res.status(201).json({"mensaje": `Producto eliminado. id: ${id}'`})
 })
